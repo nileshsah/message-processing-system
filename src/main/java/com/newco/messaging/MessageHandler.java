92247462af3c9804eb1331aa8731fd1091eacd26
Nilesh Sah <nilesh.sah13@gmail.com>
2019-12-25 10:59:46 +0100

    Add MessageHandler interface

---
+++ src/main/java/com/newco/messaging/MessageHandler.java
@@ -0,0 +1,9 @@
+package com.newco.messaging;
+
+import com.newco.messaging.model.Message;
+
+public interface MessageHandler {
+  void messageReceived(Message message);
+  void channelCreated(String channelId);
+  void channelDestroyed(String channelId);
+}
